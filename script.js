1fb0c795a282eae1305cc69e5184f859a23aa6e1
Lucia Garavito <gara0066@algonquinlive.com>
2023-02-13 17:23:49 -0500

    change id name

---
+++ script.js
@@ -0,0 +1,8 @@
+//**FUNCTION #1 setTitle */
+const $title = document.getElementById('title')
+function setTitle(newTitle) {
+    $title.textContent = 'newTitle'
+}
+console.log ($title)
+
+//**FUNCTION #2 setDescription */
