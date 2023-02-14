babfcfc3c787b608cce4f79a352c3a2c13405c3f
Lucia Garavito <gara0066@algonquinlive.com>
2023-02-13 21:42:07 -0500

    adds function to affect the theme

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 //**FUNCTION #1 setTitle */
+
 const $title = document.getElementById('title')
-function setTitle(newTitle) {
-    $title.textContent = 'newTitle'
+function setTitle(HelloWorld) {
+    $title.textContent = HelloWorld
 }
 console.log ($title)
 
 //**FUNCTION #2 setDescription */
+
+const $parragraph = document.getElementById('parragraph')
+function setDescription(newText) {
+    $parragraph.textContent = newText
+}
+
+//**FUNCTION #3 setBackgroundColor */
+
+function setBackgroundColor(color) {
+    document.body.style.backgroundColor = color;
+}
+//**FUNCTION #4 setFontColor */
+
+function setFontColor(newColor) {
+    document.body.style.color = newColor;
+}
+
+//**FUNCTION #5 setTheme */
+
+const $body = document.getElementById('body')
+
+function setTheme() {
+   $body.classList.toggle('dark-theme');
+   $body.classList.toggle('light-theme')
+    
+}
+
